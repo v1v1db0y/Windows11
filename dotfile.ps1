fb27e21121bfecbc380a5bedce93673faa8148f7
v1v1db0y <115068570+v1v1db0y@users.noreply.github.com>
2023-04-03 06:41:13 +0900

    Update dotfile.ps1

--- dotfile.ps1
+++ dotfile.ps1
@@ -1,32 +1,39 @@
-get-appxpackage microsoft.windowscommunicationsapps    | remove-appxpackage
-get-appxpackage Microsoft.XboxSpeechToTextOverlay      | remove-appxpackage
-get-appxpackage Microsoft.MicrosoftSolitaireCollection | remove-appxpackage
-get-appxpackage MicrosoftTeams                         | remove-appxpackage                          
-get-appxpackage SpotifyAB.SpotifyMusic                 | remove-appxpackage
-get-appxpackage Microsoft.Xbox.TCUI                    | remove-appxpackage
-get-appxpackage Microsoft.XboxGameOverlay              | remove-appxpackage
-get-appxpackage Microsoft.XboxIdentityProvider         | remove-appxpackage
-get-appxpackage Microsoft.MicrosoftOfficeHub           | remove-appxpackage
-get-appxpackage Microsoft.XboxGamingOverlay            | remove-appxpackage
-get-appxpackage MicrosoftCorporationII.QuickAssist     | remove-appxpackage
-get-appxpackage Microsoft.WindowsMaps                  | remove-appxpackage
-get-appxpackage Microsoft.BingNews                     | remove-appxpackage
-get-appxpackage Microsoft.GamingApp                    | remove-appxpackage
-get-appxpackage Microsoft.People                       | remove-appxpackage
-get-appxpackage Microsoft.Getstarted                   | remove-appxpackage
-get-appxpackage Clipchamp.Clipchamp                    | remove-appxpackage
-get-appxpackage Microsoft.MicrosoftStickyNotes         | remove-appxpackage
-get-appxpackage Microsoft.BingWeather                  | remove-appxpackage
-get-appxpackage Microsoft.PowerAutomateDesktop         | remove-appxpackage
-get-appxpackage Microsoft.WindowsFeedbackHub           | remove-appxpackage
-get-appxpackage Microsoft.ScreenSketch                 | remove-appxpackage
-get-appxpackage Microsoft.Todos                        | remove-appxpackage
-get-appxpackage Microsoft.GetHelp                      | remove-appxpackage
-get-appxpackage Microsoft.WindowsSoundRecorder         | remove-appxpackage
-get-appxpackage Microsoft.WindowsNotepad               | remove-appxpackage
-get-appxpackage Microsoft.WindowsAlarms                | remove-appxpackage
-get-appxpackage Microsoft.YourPhone                    | remove-appxpackage
-get-appxpackage Microsoft.MicrosoftEdge                | remove-appxpackage
+$Uninstall_Lists = @(
+          'microsoft.windowscommunicationsapps';
+          'Microsoft.XboxSpeechToTextOverlay';
+          'Microsoft.MicrosoftSolitaireCollect';
+          'MicrosoftTeams';                          
+          'SpotifyAB.SpotifyMusic';
+          'Microsoft.Xbox.TCUI';
+          'Microsoft.XboxGameOverlay';
+          'Microsoft.XboxIdentityProvider';
+          'Microsoft.MicrosoftOfficeHub';
+          'Microsoft.XboxGamingOverlay';
+          'MicrosoftCorporationII.QuickAssist';
+          'Microsoft.WindowsMaps';
+          'Microsoft.BingNews';
+          'Microsoft.GamingApp';
+          'Microsoft.People';
+          'Microsoft.Getstarted';
+          'Clipchamp.Clipchamp';
+          'Microsoft.MicrosoftStickyNotes';
+          'Microsoft.BingWeather';
+          'Microsoft.PowerAutomateDesktop';
+          'Microsoft.WindowsFeedbackHub';
+          'Microsoft.ScreenSketch';
+          'Microsoft.Todos';
+          'Microsoft.GetHelp';
+          'Microsoft.WindowsSoundRecorder';
+          'Microsoft.WindowsNotepad';
+          'Microsoft.WindowsAlarms';
+          'Microsoft.YourPhone';
+          'Microsoft.MicrosoftEdge';
+);
+ 
+# for でループしてみる。
+for ($i=0; $i -lt $Uninstall_Lists.Count; $i++){
+  get-appxpackage $Uninstall_Lists[$i] | remove-appxpackage
+} 
 
 #タスクバーを上にする
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3" /v Settings /t REG_BINARY /d 30000000feffffff0200000001000000300000002000000000000000c203000080070000e20300006000000001000000 /f
@@ -50,19 +57,14 @@ Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-
 Enable-WindowsOptionalFeature -Online -FeatureName "VirtualMachinePlatform" -All
 Enable-WindowsOptionalFeature -Online -FeatureName "Windows-Defender-Default-Definitions" -All
 
-
 #WSLを有効化
 wsl --install
 
 #WSL2に昇華
 wsl --set-default-version 2
 
-
 #Install Windows Terminal Preview for Winget
 winget install Microsoft.WindowsTerminal.Preview
-
-#Install Git
-winget install Git.Git
 
 #Uninstall Taskbar wigget
 winget uninstall "windows web experience pack"
